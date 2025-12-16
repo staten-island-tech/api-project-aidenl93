0467d050d0a50dbc1c05469d81df089c0a31bf4b
aidenl93 <aidenl93@nycstudents.net>
2025-12-16 12:30:13 -0500

    eaglercraft

---
+++ poke.js
@@ -0,0 +1,16 @@
+async function getData(poke) {
+  try {
+    // get data from api
+    const response = await fetch(`https://pokeapi.co/api/v2/pokemon/${poke}`);
+    if (response.status != 200) {
+      throw new Error(response);
+    } else {
+      const data = await response.json();
+      console.log(data);
+      document.getElementById("api-response").textContent = data.name;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+}
+getData("squirtle");
