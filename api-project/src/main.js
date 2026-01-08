48ae8895834af2bf803ef8a9eaa6d4eeb7ec4653
aidenl93 <aidenl93@nycstudents.net>
2026-01-08 13:41:18 -0500

    weiming weng bagwork anna lau

--- api-project/src/main.js
+++ api-project/src/main.js
@@ -1,19 +1,26 @@
-async function fetchCharacters(chara) {
-  try {
-    const response = await fetch(
-      `https://api.disneyapi.dev/character/${chara}`
-    );
+try {
+  const response = await fetch(`https://api.disneyapi.dev/character/`);
 
-    if (response.status != 200) {
-      throw new Error(response);
-    } else {
-      const data = await response.json();
-      console.log(data.data);
+  if (response.status != 200) {
+    throw new Error(response);
+  } else {
+    const data = await response.json();
+    console.log(data.data);
 
-      document.getElementById("api-response").textContent = data.name;
-    }
-  } catch (error) {
-    console.log(error);
+    document.getElementById("api-response").textContent = data.name;
   }
+} catch (error) {
+  console.log(error);
 }
-fetchCharacters("308");
+
+const container = document.querySelector(".section");
+function inject(item) {
+  container.insertAdjacentHTML(
+    "afterbegin",
+    `
+    <h2></h2>
+    <h2>id: ${data.data._id}</h2>
+    <h2></h2>
+    `
+  );
+}
